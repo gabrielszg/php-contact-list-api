4855fa74af5874d397f48d2effe324052c8069e7
gabrielszg <gabrielsouza5@hotmail.com>
2023-10-24 15:40:09 -0300

    refactor: use of the company service class

--- app/controllers/CompanyController.php
+++ app/controllers/CompanyController.php
@@ -4,91 +4,77 @@ namespace ContactsApi\Controllers;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use ContactsApi\Services\CompanyService;
 
 class CompanyController {
 
   private static $table = 'company';
 
+  protected CompanyService $companyService;
+
   public function __construct(
-    protected \PDO $pdo
-  ) { }
+    protected \PDO $pdo,
+    
+  ) { 
+    $this->companyService = new CompanyService($pdo);
+  }
 
   public function findAll(ServerRequestInterface $request, ResponseInterface $response) {
-    $sql = 'SELECT * FROM '.self::$table;
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute();
+    try {
+      $companies = $this->companyService->findAll();
 
-    if ($stmt->rowCount() < 1) {
+      $response->getBody()->write(json_encode($companies));
+      return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+    }catch (\Exception $e) {
       throw new \Exception("Nenhuma empresa encontrada!");
     }
-
-    $company = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
-    $response->getBody()->write(json_encode($company));
-
-    return $response
-      ->withHeader('Content-Type', 'application/json')
-      ->withStatus(200);
   }
 
   public function create(ServerRequestInterface $request, ResponseInterface $response) {
-    $args = (array) $request->getParsedBody();
-    
-    $sql = 'INSERT INTO ' . self::$table . ' (name) VALUES (:name)';
-    $stmt = $this->pdo->prepare($sql);
-    
-    if (isset($args) && !empty($args['name'])) {
-      $stmt->bindValue(':name', $args['name']);
-      $stmt->execute();
-    }
+    $company = (array) $request->getParsedBody();
 
-    if ($stmt->rowCount() < 0) {
+    try {
+      $companySaved = $this->companyService->save($company);
+
+      $response->getBody()->write(json_encode($companySaved));
+      return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(201);
+    }catch (\Exception $e) {
       throw new \Exception("Falha ao cadastrar empresa!");
     }
-
-    $response->getBody()->write(json_encode($args));
-
-    return $response
-      ->withHeader('Content-Type', 'application/json')
-      ->withStatus(201);
   }
 
   public function update(ServerRequestInterface $request, ResponseInterface $response, array $args) {
     $company = (array) $request->getParsedBody();
+    $id = intval($args['id']);
 
-    $sql = 'UPDATE '.self::$table.' SET name = :name WHERE id = :id';
+    try {
+      $updatedCompany = $this->companyService->update($company, $id);
 
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->bindValue(':name', $company['name']);
-    $stmt->bindValue(':id', $args['id']);
-    $stmt->execute();
-    
-    if ($stmt->rowCount() < 0) {
+      $response->getBody()->write(json_encode($updatedCompany));
+      return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+    }catch (\Exception $e) {
       throw new \Exception("Falha ao atualizar empresa!");
     }
-    
-    $response->getBody()->write(json_encode($company));
-
-    return $response
-      ->withHeader('Content-Type', 'application/json')
-      ->withStatus(200);
   }
 
   public function delete(ServerRequestInterface $request, ResponseInterface $response, array $args) { 
-    $sql = 'DELETE FROM '.self::$table.' WHERE id = :id';
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->bindValue(':id', $args['id']);
-    $stmt->execute();
+    $id = intval($args['id']);
+    
+    try {
+      $this->companyService->delete($id);
 
-    if ($stmt->rowCount() < 1) { 
-      throw new \Exception('Erro ao Deletar empresa!');
-    }
-
-    $stmt->closeCursor();
-
-    $response->getBody()->write(json_encode('Empresa deletada com sucesso!'));
-    return $response
+      $response->getBody()->write(json_encode('Empresa deletada com sucesso!'));
+      return $response
                 ->withHeader('Content-Type', 'application/json')
                 ->withStatus(404);
+    }catch (\Exception $e) {
+      throw new \Exception('Erro ao Deletar empresa!');
+    }
   }
 }
