c10cb53427e0973f9392734e6b63b61540fd5df0
gabrielszg <gabrielsouza5@hotmail.com>
2023-10-25 14:12:05 -0300

    feat: responsible for business rules

---
+++ app/services/ContactService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ContactsApi\Services;
+
+use ContactsApi\Repositories\ContactRepository;
+
+class ContactService {
+
+    private ContactRepository $contactRepository;
+
+    public function __construct(
+        protected \PDO $pdo
+    ) { 
+        $this->contactRepository = new ContactRepository($pdo);
+    }
+
+    public function findAllOrFilterByParam(array $params) {
+        return $this->contactRepository->findAllOrFilterByParam($params);
+    }
+
+    public function save(array $contact) {
+        return $this->contactRepository->save($contact);
+    }
+
+    public function update(array $contact, int $id) {
+        return $this->contactRepository->update($contact, $id);
+    }
+
+    public function delete(int $id) {
+        return $this->contactRepository->delete($id);
+    }
+}
